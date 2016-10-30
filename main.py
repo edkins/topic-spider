f534a91c414d1b65624376f7a197d2e5b1fcb036
Giles Edkins <gedkins@bluecatnetworks.com>
2016-10-30 16:11:51 -0400

    Count words and divide by corpus frequency

---
+++ main.py
@@ -0,0 +1,52 @@
+import urllib.request
+import nltk
+import collections
+from bs4 import BeautifulSoup
+from bs4 import element
+
+def buildSoupString(soup, indent):
+	result = ''
+	if type(soup) is element.Tag or type(soup) is BeautifulSoup:
+		if soup.name != 'script':
+			for child in soup.contents:
+				result += buildSoupString(child, indent+1)
+			return result
+	elif type(soup) is element.NavigableString:
+		string = str(soup)
+		if not string.startswith('[') or not string.endswith(']'):
+			return str(soup).replace('\n','. ') + ' '
+	else:
+		pass
+	return ''
+
+def acceptToken(token):
+	return token[0].isalpha()
+
+def corpusFrequencies():
+	counts = collections.Counter()
+	for fileid in nltk.corpus.brown.fileids():
+		for word in nltk.corpus.brown.words(fileid):
+			if acceptToken(word):
+				counts[word.lower()] += 1
+	return counts
+
+corpusFreq = corpusFrequencies()
+
+html = urllib.request.urlopen('https://en.wikipedia.org/wiki/Effective_altruism').read().decode('utf-8')
+soup = BeautifulSoup(html,'html.parser')
+
+text = ' '.join(buildSoupString(soup,0).split())
+
+tokens = nltk.word_tokenize(text)
+counts = collections.Counter()
+for token in tokens:
+	if acceptToken(token):
+		counts[token.lower()] += 1
+
+listResult = []
+for token in counts:
+	score = -counts[token] / (corpusFreq[token] + 1)
+	listResult.append((score, token))
+
+for (score,word) in sorted(listResult)[0:200]:
+	print(word + ' ' * (20 - len(word)) + '*' * int(-2*score))
